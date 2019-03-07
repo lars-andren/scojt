161385f534d9976164e4616f226bef19389a59e9
lars-andren <andren.lars@gmail.com>
2019-03-07 13:46:19 +0100

    First installment

---
+++ server.js
@@ -0,0 +1,16 @@
+/**
+ * Created by andrenlars on 07/03/19.
+ */
+
+var http = require('http');
+
+
+var server = http.createServer(function(req, res) {
+
+  res.writeHead(200);
+
+  res.end('Hi everybody!');
+
+});
+
+server.listen(8080);
