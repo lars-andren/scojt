0893d6b0b24e39cedf3b50212c1a64b0272e60d4
lars-andren <andren.lars@gmail.com>
2019-03-08 10:32:59 +0100

    Read credentials from file

--- server.js
+++ server.js
@@ -6,10 +6,44 @@ var http = require('http');
 const express = require('express');
 const path = require('path');
 
+var username = '';
+var password = '';
+
+var fs = require('fs');
+fs.readFile('resources/credentials', 'utf8', function (err,data) {
+
+  var arr = data.split(";").map(val => Number(val) + 1)
+  username = arr[0];
+  password = arr[1];
+  if (err) { return console.log(err); }
+});
+
+var EasySoap = require('easysoap');
+
+var params = {
+  host: 'api2.mypasscode.com',
+  path: '/api/CustomerApi30/',
+  wsdl: '/api/CustomerApi30?wsdl',
+
+  // set soap headers (optional)
+  headers: [{
+    'username' : username,
+    'password' : password
+  }]
+};
+
+console.log(params);
+
+var soapClient = EasySoap(params);
+//soapClient.getAllFunctions().then((functionArray) => { console.log(functionArray); })
+//.catch((err) => { throw new Error(err); });
+
 const server = express();
 
 server.set('views', path.join(__dirname, 'views'));
 server.set('view engine', 'pug');
+
+
 
 server.get('/', function(req, res){
   res.render('index');
